fedeef7938794ff4d767963dc7f129e591386c99
Karim Moradi <kmoradi.ai@gmail.com>
2023-07-14 20:39:56 +0200

    init

---
+++ producer.py
@@ -0,0 +1,10 @@
+from pulsar import Client
+
+client = Client('pulsar://localhost:6650')
+producer = client.create_producer('my-topic')
+
+for i in range(10):
+    producer.send(('hello-pulsar-%d' % i).encode('utf-8'))
+
+client.close()
+
